073fda07bc9102290e25881dbebcb6a1ca4a1fe0
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2013-12-05 09:38:24 +0400

    ~use test

---
+++ compiler/testData/codegen/boxInline/use/1.kt
@@ -0,0 +1,34 @@
+public class Input : Closeable() {
+    public fun data() : Int = 100
+}
+public  class Output : Closeable() {
+    public fun doOutput(data: Int): Int = data
+}
+
+public open class Closeable {
+    open public fun close() {}
+}
+
+public inline fun <T: Closeable, R> T.use(block: (T)-> R) : R {
+    var closed = false
+    try {
+        return block(this)
+    } catch (e: Exception) {
+        closed = true
+        try {
+            this.close()
+        } catch (closeException: Exception) {
+
+        }
+        throw e
+    } finally {
+        if (!closed) {
+            this.close()
+        }
+    }
+}
+
+
+public fun Input.copyTo(output: Output): Long {
+    return output.doOutput(this.data()).toLong()
+}
---
+++ compiler/testData/codegen/boxInline/use/2.kt
@@ -0,0 +1,17 @@
+fun test1() : Long  {
+    val input = Input()
+    return input.use<Input, Long>{
+        val output = Output()
+        output.use<Output,Long>{
+            input.copyTo(output)
+        }
+    }
+}
+
+
+fun box(): String {
+
+    if (test1() != 100.toLong()) return "test1: ${test1()}"
+
+    return "OK"
+}
