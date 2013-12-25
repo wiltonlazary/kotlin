94e2225af1b2d246169003fc2b22fa5bdc0a6ccb
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-12-25 16:36:55 +0400

    Add getPackageFragment() to JavaDescriptorResolver

--- idea/src/org/jetbrains/jet/plugin/libraries/DecompiledDataFactory.java
+++ idea/src/org/jetbrains/jet/plugin/libraries/DecompiledDataFactory.java
@@ -85,7 +85,7 @@ public final class DecompiledDataFactory {
         appendDecompiledTextAndPackageName(packageFqName);
         SerializedDataHeader.Kind kind = classFileHeader.getKind();
         if (kind == SerializedDataHeader.Kind.PACKAGE) {
-            PackageFragmentDescriptor pf = javaDescriptorResolver.getPackageFragmentProvider().getPackageFragment(packageFqName);
+            PackageFragmentDescriptor pf = javaDescriptorResolver.getPackageFragment(packageFqName);
             if (pf != null) {
                 for (DeclarationDescriptor member : sortDeclarations(pf.getMemberScope().getAllDescriptors())) {
                     if (!(member instanceof ClassDescriptor)) {
