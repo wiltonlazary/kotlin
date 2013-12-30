b764d28017e9efdc91a9b165833ded6441dfa6a6
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2013-12-30 16:40:16 +0400

    Fix syntax error in test

--- idea/testData/findUsages/java/findJavaClassUsages/JKClassDerivedAnonymousObjects.1.kt
+++ idea/testData/findUsages/java/findJavaClassUsages/JKClassDerivedAnonymousObjects.1.kt
@@ -1,12 +1,13 @@
 fun foo() {
     public trait T: A
 
-    val a = object: A()
+    val a = object: A() {
+        fun zoo() {
+            val c = object: T
+        }
+    }
 
     fun bar() {
         val b = object: T
     }
 }
-
-
-
